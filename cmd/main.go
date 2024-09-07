d48f912c59b60a5578e0ac8569f5146b46c8d17d
maksim <make.oynoshev@gmail.com>
2024-09-07 12:31:44 +0300

    asdabskdjghadkjsfh

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/macoyshev/todolist/internal/tasks"
+)
+
+func main() {
+	srv := http.NewServeMux()
+
+	srv.HandleFunc("/gTasks", tasks.gTasks)
+	srv.ListenAndServe()
+}
